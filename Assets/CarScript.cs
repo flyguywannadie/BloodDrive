7156eeb506e1285742e1448f866ab4e7a5a5e783
flyguywannadie <60896082+flyguywannadie@users.noreply.github.com>
2024-04-26 15:24:05 -0600

    Added a few things

--- Assets/CarScript.cs
+++ Assets/CarScript.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarScript : MonoBehaviour
 {
+    [SerializeField] float speed;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float friction;
+    [SerializeField] float turnspeed;
+    [SerializeField] float gravity;
+    [SerializeField] float floatingHeight = 0.25f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,30 @@ public class CarScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (!Physics.Raycast(transform.position, -transform.up, out RaycastHit ray))
+        {
+			Debug.Log("GRAVITY IS A HARNESS");
+		} 
+        else
+        {
+            Debug.Log("Float On ground NOW!!!");
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+        }
+		if (Input.GetKey(KeyCode.W))
+		{
+
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+
+		}
+	}
 }
