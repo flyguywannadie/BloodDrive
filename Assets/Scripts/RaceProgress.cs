39bddbef2e9710680cf81904a795555154180d37
JaydenSmith43 <123499904+JaydenSmith43@users.noreply.github.com>
2024-04-26 22:07:13 -0600

    racepoints half done

--- Assets/Scripts/RaceProgress.cs
+++ Assets/Scripts/RaceProgress.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class RaceProgress : MonoBehaviour
 {
+    [SerializeField] List<GameObject> racePoints;
+    int currentPoint = 0;
+
     void Start()
     {
-        
+        for (int i = 1; i < racePoints.Count; i++) //everything after the first element
+        {
+            racePoints[i].SetActive(false);
+        }
     }
 
-    void Update()
-    {
-        
-    }
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Point")
+        {
+            racePoints[currentPoint].SetActive(false);
+            currentPoint++;
+            if(currentPoint == racePoints.Count)
+            {
+                currentPoint = 0;
+            }
+
+			racePoints[currentPoint].SetActive(true);
+		}
+	}
 }
